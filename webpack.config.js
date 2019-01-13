3aa121a51238301f469c1d41b7d0835ef043602b
alucas <alucas@redpoints.com>
2019-01-13 19:31:07 +0100

    ADD webpack scripts

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack Configuration (Client & Server)
+
+import clientConfig from '.webpack/webpack.config.client';
+import serverConfig from '.webpack/webpack.config.server';
+
+export default [
+  clientConfig,
+  serverConfig
+];
